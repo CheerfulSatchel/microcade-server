91761ed7917878987210be62d7a1181a62cfcd8f
James Wu <jamwu@microsoft.com>
2020-07-27 18:18:00 -0400

    Updated dependencies and server index.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import path from "path";
 
+import * as http from "http";
+import socket from "socket.io";
+import ip from "ip";
+
+import { RoomManager, Room } from "./services/RoomManager";
+
 const app = express();
-const port = 3001;
+const server: http.Server = http.createServer(app);
+const io: socket.Server = socket(server);
+const roomManager: RoomManager = new RoomManager();
+
+const port: number = 3001;
 
 app.use(express.static(path.join(__dirname, "../out/client")));
 
@@ -12,9 +22,38 @@ app.get("/api", (req, res) => {
   res.send("Ur mufm");
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
+// creates a room
+app.post("/api/createRoom", (req, res) => {
+  roomManager.createNewRoom().then((roomId: string) => {
+    res.send({ roomId, websocketURL: getWebsocketURL(roomId) });
+  });
+});
+
+// deletes a room
+app.delete("/api/room/:roomId", (req, res) => {
+  res.send({ room: roomManager.deleteRoom });
+});
+
+// lists all rooms
+app.get("/api/rooms/list", (req, res) => {
+  const allRooms: Record<string, Room> = roomManager.getAllRooms();
+
+  res.send({ allRooms });
+});
+
+// get a specific room
+app.get("/api/room/:roomId", (req, res) => {
+  res.send({ room: req.params.roomId });
+});
+
+io.on("connection", (socket: socket.Socket) => {
+  console.log("AYYY MACARENA");
+});
+
+server.listen(port, "127.0.0.1", () => {
   return console.log(`server is listening on ${port}`);
 });
+
+function getWebsocketURL(roomId: string) {
+  return `ws://${ip.address()}:3001/room/${roomId}`;
+}
---
+++ src/server/services/RoomManager.ts
@@ -0,0 +1,69 @@
+import generate from "adjective-adjective-animal";
+
+export interface Room {
+  created: Date;
+  name: string;
+  users: string[];
+  chatHistory: [];
+}
+
+export class RoomManager {
+  private rooms: Record<string, Room> = {};
+
+  private createRoom(roomId: string) {
+    const newRoom: Room = {
+      created: new Date(),
+      name: roomId,
+      users: [],
+      chatHistory: [],
+    };
+
+    this.rooms[roomId] = newRoom;
+  }
+
+  public async createNewRoom(): Promise<string> {
+    let roomId = await generate("pascal");
+    // Highly unlikely. Go buy a lottery ticket if you manage to use a previously generated room name.
+    while (this.rooms[roomId]) {
+      roomId = await generate("pascal");
+    }
+
+    this.createRoom(roomId);
+
+    return roomId;
+  }
+
+  public getAllRooms() {
+    return this.rooms;
+  }
+
+  public removePlayerFromRoom(roomId: string, playerIdToRemove: string) {
+    if (this.getRoom(roomId)) {
+      if (this.getRoom(roomId).users.includes(playerIdToRemove)) {
+        delete this.getRoom(roomId).users[playerIdToRemove];
+      }
+    }
+  }
+
+  public deleteRoom(roomId: string) {
+    if (this.getRoom(roomId)) {
+      delete this.getRoom[roomId];
+    }
+  }
+
+  public getRoom(roomId: string): Room {
+    return this.rooms[roomId];
+  }
+
+  public addUserToRoom(roomId: string, userId: string) {
+    if (!this.rooms[roomId].users.includes(userId)) {
+      this.rooms[roomId].users.push(userId);
+    }
+  }
+
+  public removeRoomIfEmpty(roomId: string) {
+    if (this.rooms[roomId].users.length === 0) {
+      delete this.rooms[roomId];
+    }
+  }
+}
