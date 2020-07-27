1f5f04900ab49134d2cec70f74a8dbed0f1d4d72
Samuel Kwak <sakwak@microsoft.com>
2020-07-27 12:26:31 -0400

    update to shared convention

--- src/HelloWorld.ts
+++ src/HelloWorld.ts
@@ -1,15 +1,15 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Ur mum")
-})
+  res.send("Ur mum");
+});
 
 app.listen(port, (err) => {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
-  return console.log(`server is listening on ${port}`)
-})
+  return console.log(`server is listening on ${port}`);
+});
