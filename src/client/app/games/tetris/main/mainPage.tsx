4885fba1ec133aed19f31efbadd4cfae3e6f1a3b
Samuel Kwak <sakwak@microsoft.com>
2020-07-27 21:07:17 -0400

    more basic main page styling

--- src/client/app/games/tetris/main/mainPage.tsx
+++ src/client/app/games/tetris/main/mainPage.tsx
@@ -24,6 +24,29 @@ const RoomsList = styled.ul``;
 
 const JoinRoomTextbox = styled.input``;
 
+const Content = styled.div``;
+
+const Room = styled.li`
+  list-style: none;
+  background-color: #f9f9f9;
+  border-bottom: 1px #151515 solid;
+  width: 600px;
+  margin-bottom: 0;
+  padding: 16px;
+  color: #151515;
+
+  & p {
+    margin-bottom: 0;
+  }
+`;
+
+const RoomName = styled.h4`
+  font-size: 18px;
+  margin-top: 0;
+`;
+
+const RoomCreated = styled.p``;
+
 const TetrisMainPage: React.FC<TetrisMainPage.Props> = ({ userName }) => {
   const [roomsList, setRoomsList] = useState<TetrisMainPage.IndexedRoomlist>({});
 
@@ -41,21 +64,32 @@ const TetrisMainPage: React.FC<TetrisMainPage.Props> = ({ userName }) => {
   return (
     <div>
       <h2>Hello {userName}</h2>
-      <CreateRoomButton onClick={createRoom}>Create a room</CreateRoomButton>
-      <RoomsList>
-        {Object.keys(roomsList).map((roomKey) => {
-          const room = roomsList[roomKey];
+      <Content>
+        <div>
+          <CreateRoomButton onClick={createRoom}>Create a room</CreateRoomButton>
+        </div>
+        <div>
+          <p>Join a room</p>
+          <JoinRoomTextbox type="text" placeholder="Room name" />
+        </div>
+        <RoomsList>
+          <h3>Available Rooms</h3>
+          {Object.keys(roomsList).map((roomKey) => {
+            const room = roomsList[roomKey];
+            const createdDate = new Date(room.created);
 
-          return (
-            <li key={room.name}>
-              <p>{room.name}</p>
-              <p>{new Date(room.created).toLocaleDateString()}</p>
-              <p>Users: {room.users?.length}</p>
-            </li>
-          );
-        })}
-      </RoomsList>
-      <JoinRoomTextbox type="text" />
+            return (
+              <Room key={room.name}>
+                <RoomName>{room.name}</RoomName>
+                <RoomCreated>
+                  Created: {createdDate.toLocaleDateString()} {createdDate.toLocaleTimeString()}
+                </RoomCreated>
+                <p>Users: {room.users?.length}</p>
+              </Room>
+            );
+          })}
+        </RoomsList>
+      </Content>
     </div>
   );
 };
